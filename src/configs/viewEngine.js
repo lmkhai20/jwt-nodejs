b0abf56219cfd100b61989b334318a87db4bee69
lmkhai20 <leminhkhai2000@gmail.com>
2025-05-31 23:34:51 +0700

    mvc

--- src/configs/viewEngine.js
+++ src/configs/viewEngine.js
@@ -3,7 +3,7 @@ import express from 'express';
 const configViewEngine = (app) => {
     app.use(express.static('./src/public'));
     app.set('view engine', 'ejs');
-    app.set('views', './ser/views');
+    app.set('views', './src/views');
 }
 
 export default configViewEngine;
